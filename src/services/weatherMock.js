874e7c20f4546eeec5825f7b543b8c645ce95008
Gabi Draghici <draghici.gabi89@gmail.com>
2024-03-17 11:48:37 +0000

    Mock api call pseudocode added

---
+++ src/services/weatherMock.js
@@ -0,0 +1,17 @@
+
+// !'For mocking API calls specifically, I would recommend using Sinon in conjunction with Mocha for testing. ' - from xpert assistant!
+
+// TODO: Create api mock using jest/sinon? 
+
+// TODO: Install package npm i sinon npm i jest
+
+// TODO: Define var that requires sinon testing module (use axios - require axios/use mocha - require chai)
+
+// TODO: Write API call function
+
+// TODO: Create Sinon stub to mock the API call
+
+// TODO: Call the function that makes the weather-js API call
+
+// TODO: Restore the original weather.find method
+
