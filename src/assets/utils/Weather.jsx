69f642cdee446dbcdcbf9dd753dc838f626c2822
Jean-003 <amasterpiece02@gmail.com>
2024-03-17 18:03:10 +0000

    re-installed weather-js and added initial code to the weather.jsx file

--- src/assets/utils/Weather.jsx
+++ src/assets/utils/Weather.jsx
@@ -1,44 +1,11 @@
-import ReactWeather, { useOpenWeather } from "react-open-weather";
-import {
-  setKey,
-  setDefaults,
-  fromAddress,
-  geocode,
-  RequestType,
-} from "react-geocode";
+import weather from 'weather-js';
 
-const Weather = () => {
-  
-  setDefaults({
-    key: "AIzaSyC9qEaUqvbwazBZScpgJ_CZcXU5jbF1OU4", 
-    language: "en", 
-  });
-
-  // Getting latitude & longitude from address.
-  fromAddress("Eiffel Tower")
-    .then(({ results }) => {
-      const { lat, lng } = results[0].geometry.location;
-      console.log(lat, lng);
-      const { data, isLoading, errorMessage } = useOpenWeather({
-        key: "cf98f413c40be2f82a74ed0fd421bb0c",
-        lat: lat,
-        lon: lon,
-        lang: "en",
-        unit: "metric", 
-      });
-      return (
-        <ReactWeather
-          isLoading={isLoading}
-          errorMessage={errorMessage}
-          data={data}
-          lang="en"
-          locationLabel="Munich"
-          unitsLabels={{ temperature: "C", windSpeed: "Km/h" }}
-          showForecast
-        />
-      );
-    })
-    .catch(console.error);
-};
+weather.find({ search: 'San Francisco, CA', degreeType: 'F' }, function(err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(JSON.stringify(result, null, 2));
+    }
+});
 
 export default Weather;
