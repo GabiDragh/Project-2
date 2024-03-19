7a1cf111ba7da61c9014b1b10d2a5904bfd73169
Jean-003 <amasterpiece02@gmail.com>
2024-03-19 19:17:48 +0000

    trying to work with React Open Weather and react-geocode

--- src/assets/utils/Weather.jsx
+++ src/assets/utils/Weather.jsx
@@ -1,25 +1,44 @@
-import ReactWeather, { useOpenWeather } from 'react-open-weather';
+import ReactWeather, { useOpenWeather } from "react-open-weather";
+import {
+  setKey,
+  setDefaults,
+  fromAddress,
+  geocode,
+  RequestType,
+} from "react-geocode";
 
 const Weather = () => {
-  const { data, isLoading, errorMessage } = useOpenWeather({
-    key: 'cf98f413c40be2f82a74ed0fd421bb0c',
-    lat: '48.137154',
-    lon: '11.576124',
-    lang: 'en',
-    unit: 'metric', // values are (metric, standard, imperial)
+  
+  setDefaults({
+    key: "AIzaSyC9qEaUqvbwazBZScpgJ_CZcXU5jbF1OU4", 
+    language: "en", 
   });
-  return (
-    <ReactWeather
-      isLoading={isLoading}
-      errorMessage={errorMessage}
-      data={data}
-      lang="en"
-      locationLabel="Munich"
-      unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
-      showForecast
-    />
-  );
+
+  // Getting latitude & longitude from address.
+  fromAddress("Eiffel Tower")
+    .then(({ results }) => {
+      const { lat, lng } = results[0].geometry.location;
+      console.log(lat, lng);
+      const { data, isLoading, errorMessage } = useOpenWeather({
+        key: "cf98f413c40be2f82a74ed0fd421bb0c",
+        lat: lat,
+        lon: lon,
+        lang: "en",
+        unit: "metric", 
+      });
+      return (
+        <ReactWeather
+          isLoading={isLoading}
+          errorMessage={errorMessage}
+          data={data}
+          lang="en"
+          locationLabel="Munich"
+          unitsLabels={{ temperature: "C", windSpeed: "Km/h" }}
+          showForecast
+        />
+      );
+    })
+    .catch(console.error);
 };
 
-
-export default Weather
+export default Weather;
