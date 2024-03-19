ae840c39380fe424082c8713b755f8541debc144
Gabi Draghici <draghici.gabi89@gmail.com>
2024-03-19 01:03:06 +0000

    Message and buttons added to scene

--- src/assets/Scenes/RainScene.jsx
+++ src/assets/Scenes/RainScene.jsx
@@ -2,11 +2,12 @@
 
 import React, {useRef, useEffect } from 'react' //Import React and hooks from React
 import * as THREE from 'three' // Import Three.js
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader' // Import gltf loader to load avatar
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader' // Import gltf loader to load avatar - not using it since I created the separate component
 import RainBackground from '../images/RainBackground.jpg'
 import RainAvatar  from './RainAvatar'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+
 // import avatarRunning from '../../../public/models/Running.glb'
 
 
@@ -26,6 +27,7 @@ useEffect(() => {
     rendererRef.current = renderer;
     document.getElementById('hero').appendChild ( renderer.domElement ); //add the rendered element to the HTML (canvas element)
 
+    // DONE: Add scene background - background image added
     // Add background image loader
     const loader = new THREE.TextureLoader();
     loader.load(RainBackground, (texture) => {
@@ -66,40 +68,47 @@ useEffect(() => {
 
     animate();
 
-    // TODO: Add avatar
+    // TODO: Add avatar - separate component created 
 
-    // const loader = new GLTFLoader();
-    // loader.load( '/models/Running.glb', gltf => {
-    //         const avatar = gltf.scene;
-    //         avatar.scale.set(1, 1, 1);
-    //         avatar.position.set(0, 0, 0);
-    //         scene.add(avatar);
-    //     }
-    // );
-
-     // Cleanup
-     return () => {
+    // FIXME: Cleanup - need ta help with the cleanup - atm, at every save a new canvas is created, keeping the old ones, outside of the hero div (they annoyingly still render under the hero section)
+    return () => {
         if (rendererRef.current) {
-            rendererRef.current.dispose();
-        // scene.dispose();
-        }
+        rendererRef.current.dispose();
+        };
+        // scene.dispose(); - tried this to clean the canvas scene rendering extra, but not successful 
     };
 
 }, []);
 
+    // DONE:Add button handler
+
+    const handleWeatherForecastClick = () => {
+        console.log('Weather forecast component link'); //FIXME: All link to component
+    };
+
+    const handleBookRecommendationsClick = () => {
+        console.log('Book recommendations link');
+        window.open('https://www.audible.co.uk/search?keywords=rainy+day+reads&k=rainy+day+reads', '_blank');
+    };
+
   return (
 
-    // FIXME: Add scene background - background image added but looking bad with the rain
+    
     <div id="hero">
-    <Canvas
-        style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}
-        camera={{ position: [0, 2, 5], fov: 30 }}
-    >
-        <OrbitControls />
-        <ambientLight intensity={1} />
-        <RainAvatar />
-    </Canvas>
-</div>
+        <Canvas
+            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0}}
+            camera={{ position: [0, 2, 4], fov: 30 }}>
+            {/* <OrbitControls /> */} 
+            <ambientLight intensity={3} />
+            <RainAvatar style={{ position: 'absolute', bottom: '10px', left: '50%', transform: 'translateX(-50%)', zIndex: 1 }}/>
+        </Canvas>
+
+       <div style={{ position: 'absolute', bottom: '20px', left: '50%', transform: 'translateX(-50%)', textAlign: 'center' }}>
+                <p style={{ fontSize: '24px', marginBottom: '20px' }}>Oh, no! It is pouring out here! Better stay indoors for now! Would you like to see the weather forecast or get book recommendations?</p>
+                <button onClick={handleWeatherForecastClick} style={{ fontSize: '18px', marginRight: '10px' }}>Weather Forecast</button>
+                <button onClick={handleBookRecommendationsClick} style={{ fontSize: '18px' }}>Book Recommendations</button>
+            </div>
+    </div>
 
   );
 }
