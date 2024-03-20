983505104212f79e8abb78e0e9712b3135f6724a
Mohammed <maboobakar17@gmail.com>
2024-03-20 20:19:22 +0000

    updated all the footer code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css'; import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.css'; 
+import Footer from './Footer';
 import Hero from './Hero';
+import Footer1 from './Footer1';
+import Navbar from './Navbar/Navbar';
 // import SimpleSlider from "./components/Footer";
 
 const Home = () => (
@@ -8,7 +11,7 @@ const Home = () => (
         <Navbar/>
         <Hero/>
         <Footer/>
-       
+        <Footer1/>
     </>
 );
 
