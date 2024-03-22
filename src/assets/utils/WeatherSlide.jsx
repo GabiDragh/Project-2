0b838e37690ff04efef637673d857701cb21c4b9
Sherin Binny <sherinbinny@sherins-air.mynet>
2024-03-22 18:29:48 +0000

    img change

--- src/assets/utils/WeatherSlide.jsx
+++ src/assets/utils/WeatherSlide.jsx
@@ -6,12 +6,12 @@ const WeatherSlide = ({ forecast }) => {
   return (
     <div>
       <h2 className='date'>{forecast.date}</h2>
-      <p className='temp'>{Math.floor(forecast.temperature)}°C <img src="src/assets/images/cloud.svg"  width="50" height="10"></img></p>
+      <p className='temp'>{Math.floor(forecast.temperature)}°C <img src="src/assets/images/cloud.png"  width="50" height="10"></img></p>
       <p className='desc'>{forecast.description}</p>
       <p className='feels'>Feels like {forecast.feels_like}°C</p>
-      <p className='humid'><img src="src/assets/images/humid.svg" width="20" height="20"></img> {forecast.humidity}%</p>
-      <p className='pressure'><img src="src/assets/images/pressure.svg" width="20" height="20"></img> {forecast.pressure}</p>
-      <p className='wind'><img src="src/assets/images/wind.svg" width="20" height="20"></img> {forecast.wind_speed}m/s</p>
+      <p className='humid'><img src="src/assets/images/humidity.png" width="20" height="20"></img> {forecast.humidity}%</p>
+      <p className='pressure'><img src="src/assets/images/pressure.png" width="20" height="20"></img> {forecast.pressure}</p>
+      <p className='wind'><img src="src/assets/images/wind.png" width="20" height="20"></img> {forecast.wind_speed}m/s</p>
     </div>
   );
 };
